cb373f7caadb14b074f86e6ef6702de6428569fb
jamor <jamor.devbr@gmail.com>
2023-11-20 17:55:03 -0300

    PrPayment, one-to-one association

--- src/main/java/com/estudando/course/config/TestConfig.java
+++ src/main/java/com/estudando/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.estudando.course.entities.Category;
 import com.estudando.course.entities.Order;
 import com.estudando.course.entities.OrderItem;
+import com.estudando.course.entities.Payment;
 import com.estudando.course.entities.Product;
 import com.estudando.course.entities.User;
 import com.estudando.course.entities.enums.OrderStatus;
@@ -82,6 +83,12 @@ public class TestConfig implements CommandLineRunner{
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
 		
+		//numa relação um para um nao chama o repository
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T22:53:07Z"), o1);
+		o1.setPayment(pay1);	//associação de mao dupla em memoria: associando o pedido o1 com pagamento pay1.  
+		
+		orderRepository.save(o1); //salvando novamente o pedido e o JPA vai tratar de salvar o pagamento desse pedido
+				
 	}	
 	
 	/*essa classe vai servir para configurar os database seeding
--- src/main/java/com/estudando/course/entities/Order.java
+++ src/main/java/com/estudando/course/entities/Order.java
@@ -8,6 +8,7 @@ import java.util.Set;
 import com.estudando.course.entities.enums.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -15,6 +16,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 
 @Entity
@@ -35,8 +37,11 @@ public class Order implements Serializable {
 	@JoinColumn(name = "client_id")
 	private User client;	
 	
-	@OneToMany(mappedBy = "id.order") 
-	private Set<OrderItem> items = new HashSet<>();
+	@OneToMany(mappedBy = "id.order")
+	private Set<OrderItem> items = new HashSet<>();	
+	
+	@OneToOne(mappedBy = "order", cascade = CascadeType.ALL) //caso mapear operação OnToOne com o mesmo ID é obrigatorio colocar "cascade" 
+	private Payment payment;
 
 	public Order() {
 	}
@@ -83,6 +88,14 @@ public class Order implements Serializable {
 		this.client = client;
 	}
 	
+	public Payment getPayment() {
+		return payment;
+	}
+
+	public void setPayment(Payment payment) {
+		this.payment = payment;
+	}
+
 	public Set<OrderItem> getItems(){
 		return items;
 	}
